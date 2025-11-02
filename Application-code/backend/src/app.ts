1bc87732934f3cb17c91803b02a54390c814706e
ShantanuCodeCraft <166985322+ShantanuCodeCraft@users.noreply.github.com>
2025-11-02 10:34:55 -0800

    Readiness check to see if the server is ready to serve requests

--- Application-code/backend/src/app.ts
+++ Application-code/backend/src/app.ts
@@ -9,6 +9,7 @@ import cookieParser from "cookie-parser";
 import { auth } from "./api/middleware/auth.js";
 import gameapi from "./api/gameAPI.js";
 import dotenv from "dotenv";
+import { prisma } from "./prisma.js";
 
 dotenv.config()
 
@@ -21,16 +22,29 @@ const corsOptions = {
 const port = 8080;
 const app = express();
 const serve = createServer(app);
-export const io = new Server(serve, {
-    cors: corsOptions,
-});
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use("/api", userapi, gameapi);
 
+// Readiness check to see if the server is ready to serve requests
+app.get("/healthz", async (req, res) => {
+    try {
+        await prisma.$queryRaw`SELECT 1`;
+        res.json({ app: "ok", db: "connected" });
+    } catch {
+        res.status(500).json({ app: "ok", db: "disconnected" });
+    }
+});
+
+// Initializing game manager 
 const gameManager = new GameManager();
+
+// Initializing socket 
+export const io = new Server(serve, {
+    cors: corsOptions,
+});
 
 io.on("connection", function connection(socket) {
     const Request = socket.request as Request;
